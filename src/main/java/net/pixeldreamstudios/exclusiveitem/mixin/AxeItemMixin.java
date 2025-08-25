33bbe8f10d413d7787aafcd8e140643da87132fd
KevinCz <lenguyenhoangthanh123@gmail.com>
2025-08-25 21:43:01 +0700

    Remove unneeded injections

--- src/main/java/net/pixeldreamstudios/exclusiveitem/mixin/AxeItemMixin.java
+++ src/main/java/net/pixeldreamstudios/exclusiveitem/mixin/AxeItemMixin.java
@@ -1,6 +1,5 @@
 package net.pixeldreamstudios.exclusiveitem.mixin;
 
-import net.minecraft.block.BlockState;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemStack;
@@ -8,8 +7,6 @@ import net.minecraft.item.ItemUsageContext;
 import net.minecraft.text.Text;
 import net.minecraft.util.ActionResult;
 import net.minecraft.util.Formatting;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
 import net.pixeldreamstudios.exclusiveitem.ExclusiveItemUtil;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -31,14 +28,5 @@ public class AxeItemMixin {
         }
     }
 
-    @Inject(method = "canMine", at = @At("HEAD"), cancellable = true)
-    private void ei$restrictMine(BlockState state, World world, BlockPos pos, PlayerEntity miner, CallbackInfoReturnable<Boolean> cir) {
-        if (world.isClient) return;
 
-        ItemStack stack = miner.getMainHandStack();
-        if (!ExclusiveItemUtil.ensureOwnedForUse(stack, miner)) {
-            miner.sendMessage(Text.translatable("exclusiveitem.message.mine_with_tool_denied").formatted(Formatting.RED), true);
-            cir.setReturnValue(false);
-        }
-    }
 }
--- src/main/java/net/pixeldreamstudios/exclusiveitem/mixin/PickaxeItemMixin.java
+++ src/main/java/net/pixeldreamstudios/exclusiveitem/mixin/PickaxeItemMixin.java
@@ -1,33 +1,9 @@
 package net.pixeldreamstudios.exclusiveitem.mixin;
 
-import net.minecraft.block.BlockState;
-import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.ItemStack;
 import net.minecraft.item.PickaxeItem;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.world.World;
-import net.pixeldreamstudios.exclusiveitem.ExclusiveItemUtil;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(PickaxeItem.class)
 public class PickaxeItemMixin {
 
-    @Inject(method = "canMine", at = @At("HEAD"), cancellable = true)
-    private void ei$restrictMine(BlockState state, World world, BlockPos pos, PlayerEntity miner, CallbackInfoReturnable<Boolean> cir) {
-        if (world.isClient) return;
-
-        ItemStack stack = miner.getMainHandStack();
-        if (!ExclusiveItemUtil.ensureOwnedForUse(stack, miner)) {
-            miner.sendMessage(
-                    Text.translatable("exclusiveitem.message.mine_with_tool_denied").formatted(Formatting.RED),
-                    true
-            );
-            cir.setReturnValue(false);
-        }
-    }
 }
